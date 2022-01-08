ff1b8868c6a7752fc57ceac07183b573b5ec8762
holyc0w <subhranshupati0412@gmail.com>
2022-01-08 10:49:12 +0530

    user state(auth by firesebase) and button styling

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
-import React from 'react';
-import { Route,Switch } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.components';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.components";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
+import "./App.css";
 
-const HatsPage=()=>(
-  <div>
-    Hats!
-  </div>
-)
-const JacketsPage=()=>(
-  <div>
-    Jacket!
-  </div>
-)
-const SneakersPage=()=>(
-  <div>
-    Sneakers!
-  </div>
-)
-const WomensPage=()=>(
-  <div>
-    Womens!
-  </div>
-)
-const MensPage=()=>(
-  <div>
-    Mens!
-  </div>
-)
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
 
-function App() {
-  return (
-    <div>
-      <Route path='/' component={Header}/>
-      <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={ShopPage}/>
-      <Route exact path='/login' component={SignInSignUpPage}/>
-      <Route path='/shop/hats' component={HatsPage}/>
-      <Route path='/shop/jackets' component={JacketsPage}/>
-      <Route path='/shop/sneakers' component={SneakersPage}/>
-      <Route path='/shop/womens' component={WomensPage}/>
-      <Route path='/shop/mens' component={MensPage}/>
-      </Switch>
-    </div>
-  );
+  unsubscribeFromAuth = null; //Prevent memory leaks
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    // console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    const HatsPage = () => <div>Hats!</div>;
+    const JacketsPage = () => <div>Jacket!</div>;
+    const SneakersPage = () => <div>Sneakers!</div>;
+    const WomensPage = () => <div>Womens!</div>;
+    const MensPage = () => <div>Mens!</div>;
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/login" component={SignInSignUpPage} />
+          <Route path="/shop/hats" component={HatsPage} />
+          <Route path="/shop/jackets" component={JacketsPage} />
+          <Route path="/shop/sneakers" component={SneakersPage} />
+          <Route path="/shop/womens" component={WomensPage} />
+          <Route path="/shop/mens" component={MensPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
